8aa3850e67c3a7b7a0ee4311939e4d480018b506
Ronalyn14 <pineda.j.bscs@gmail.com>
2022-09-20 12:06:12 +0800

    rename

--- app/Models/Usertype.php
+++ app/Models/Usertype.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class usertype extends Model
+class Usertype extends Model
 {
     public $table = "usertype";
 
