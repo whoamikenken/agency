30a3dbb8a7964c961bac499348e077b46b293864
Kennedy Hipolito <whoamikenken@gmail.com>
2022-10-26 16:47:17 +0800

    Change CDN link to avoid redirect

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -70,7 +70,7 @@
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
         
         {{-- SWEET ALERT --}}
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         
         {{-- DATATABLE --}}
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
