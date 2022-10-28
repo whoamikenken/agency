027b42f026bea25bb887265e5a81c35e73160819
Kennedy Hipolito <41745628+whoamikenken@users.noreply.github.com>
2022-10-28 23:30:18 +0800

    Redirect Change Specific Controller

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use App\Http\Controllers\LoginController;
 use Throwable;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
